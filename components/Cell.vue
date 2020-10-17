a215eb5fee6941494516d502dfb137d3029fefe4
kekhuay <kekhuay@users.noreply.github.com>
2020-10-17 14:00:20 +0700

    created game board

---
+++ components/Cell.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="cell">
+    <span v-if="'' !== value">{{ value }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    value: String,
+  },
+});
+</script>
+
+<style lang="sass" scoped>
+  $size: 64px
+
+  .cell
+    align-items: center
+    border: 2px solid rgb(75, 75, 75)
+    display: flex
+    font-size: 42px
+    height: $size
+    justify-content: center
+    width: $size
+</style>
---
+++ components/TicTacToe.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="tictactoe-board">
+    <div v-for="(m, i) in 3" :key="m">
+      <div v-for="(n, j) in 3" :key="n">
+        <cell :value="board[i][j]" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data(): {} {
+    return {
+      board: [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+      ],
+    };
+  },
+});
+</script>
+
+<style lang="sass" scoped>
+  .tictactoe-board
+    display: flex
+    flex-wrap: wrap
+    height: 204px
+    width: 204px
+</style>
